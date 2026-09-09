d4fee8594e8095f8abc0c89088b15bbe825f94d4
Jyuuuuo <jihuoo1320@gmail.com>
2026-09-10 00:03:08 +0900

    S03 최종

--- Assets/S03/S03_Mesh2D.cs
+++ Assets/S03/S03_Mesh2D.cs
@@ -8,19 +8,19 @@ public class S03_ : MonoBehaviour
         Vector3[] vertices = new Vector3[]
         {
               new Vector3(0f, 1f, 0f), // 0
-              new Vector3(1f, 0f, 0f), // 1
-              new Vector3(2f, 1f, 0f), // 2
-              new Vector3(2f, 2f, 0f), // 3
-              new Vector3(1f, 3f, 0f), // 4
-              new Vector3(0f, 2f, 0f), // 5
+              new Vector3(1.732f, 0f, 0f), // 1
+              new Vector3(3.464f, 1f, 0f), // 2
+              new Vector3(3.464f, 2.732f, 0f), // 3
+              new Vector3(1.732f, 3.732f, 0f), // 4
+              new Vector3(0f, 2.732f, 0f), // 5
 
         };
         int[] triangles = new int[]
         {
             0, 1, 2,
-            2, 3, 5,
-            3, 4, 5,
-            0, 2, 5
+            0, 2, 3,
+            0, 3, 4,
+            0, 4, 5
         };
 
 
