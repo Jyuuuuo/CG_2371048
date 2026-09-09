97567d98b6435ca1109d9f6f5bdf04cf8e85fbfa
Jyuuuuo <jihuoo1320@gmail.com>
2026-09-09 23:18:27 +0900

    S03 2번째

--- Assets/S03/S03_Mesh2D.cs
+++ Assets/S03/S03_Mesh2D.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
 
-public class S03_
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_ : MonoBehaviour
 {
-    
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+              new Vector3(0f, 1f, 0f), // 0
+              new Vector3(1f, 0f, 0f), // 1
+              new Vector3(2f, 1f, 0f), // 2
+              new Vector3(2f, 2f, 0f), // 3
+              new Vector3(1f, 3f, 0f), // 4
+              new Vector3(0f, 2f, 0f), // 5
+
+        };
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            2, 3, 5,
+            3, 4, 5,
+            0, 2, 5
+        };
+
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
 }
